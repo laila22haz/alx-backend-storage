18b745970e780d639ded20d6f05e3ba78a397167
laila22haz <hazmirlaila316@gmail.com>
2024-01-18 00:17:30 +0100

    FUN WITH SQL

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- Best band ever!
 -- ranks country origins of bands
 
-SELECT origin, SUM(fans) as total_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP By origin
-ORDER By total_fans DESC;
+ORDER By nb_fans DESC;
