fc5a865cfc44a13e17a2aa03f61f96a5b46395de
laila22haz <hazmirlaila316@gmail.com>
2024-01-17 23:27:37 +0100

    FUN WITH SQL

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- In and not out
+-- creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
