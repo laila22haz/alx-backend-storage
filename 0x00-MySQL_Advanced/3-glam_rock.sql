78d6c96fb04295cfc0e72c53992811a2ca584f2f
laila22haz <hazmirlaila316@gmail.com>
2024-01-18 16:59:17 +0100

    4th try git add 3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- Old school band
+-- lists all bands with...
+
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
