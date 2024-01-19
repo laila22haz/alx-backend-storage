272f610eea3deaa58a063d1561c8666b5eacb5bd
laila22haz <hazmirlaila316@gmail.com>
2024-01-19 02:06:19 +0100

    Go Go You Can DO It

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,17 @@
 -- Safe divide
-
 DELIMITER $$
+
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
     DECLARE result FLOAT;
+    
     IF b = 0 THEN
         SET result = 0;
     ELSE
         SET result = a / b;
     END IF;
-    RETURN result
+
+    RETURN result;
 END $$
+
 DELIMITER ;
