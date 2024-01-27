dde1bd289d95904b350b24132f378b14f787a4eb
Laila_Hazmir <125223268+laila22haz@users.noreply.github.com>
2024-01-27 01:12:32 +0100

    Good things take time

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,12 +17,33 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    """call_history"""
+
+    @wraps(method)
+    def wrapper(self, *args):
+        """wrapper"""
+        input_key = method.__qualname__ + ':inputs'
+        self._redis.rpush(input_key, str(args))
+
+        resut = method(self, *args)
+
+        output_key = method.__qualname__ + ':outputs'
+
+        self._redis.rpush(output_key, str(resut))
+
+        return resut
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         self._redis = redis.Redis(host='localhost', port=6379)
         self._redis.flushdb()
-    
+
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis using a random key"""
         key = str(uuid.uuid4())
@@ -44,4 +65,3 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
         """convert the data back to the desired format"""
         return self.get(key, int)
-
