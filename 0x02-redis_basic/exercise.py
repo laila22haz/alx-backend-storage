f7dace564a61ad8bd2513a5f3f803263aafb546c
Laila_Hazmir <125223268+laila22haz@users.noreply.github.com>
2024-01-27 02:12:08 +0100

    Good things take time

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,84 +1,83 @@
 #!/usr/bin/env python3
-"""doc doc module"""
-
+"""Writing strings to Redis class"""
 import redis
 import uuid
-from typing import Union, Callable, Optional
 from functools import wraps
+from typing import Union, Callable, Optional
 
 
 def count_calls(method: Callable) -> Callable:
-    """doc doc class"""
+    """Incrementing values"""
+    key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """doc doc class"""
-        key = method.__qualname__
+        """wrapper funtion"""
         self._redis.incr(key)
         return method(self, *args, **kwargs)
-
     return wrapper
 
 
 def call_history(method: Callable) -> Callable:
-    """doc doc class"""
-    inkey = method.__qualname__ + ":inputs"
-    outkey = method.__qualname__ + ":outputs"
+    """call_history"""
 
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """doc doc class"""
-        self._redis.rpush(inkey, str(args))
-        res = method(self, *args, **kwargs)
-        self._redis.rpush(outkey, str(res))
-        return res
+    def wrapper(self, *args):
+        """wrapper"""
+        input_key = method.__qualname__ + ':inputs'
+        self._redis.rpush(input_key, str(args))
 
+        resut = method(self, *args)
+
+        output_key = method.__qualname__ + ':outputs'
+
+        self._redis.rpush(output_key, str(resut))
+
+        return resut
     return wrapper
 
 
-def replay(method: Callable) -> None:
-    """doc doc class"""
+def repaly(method: Callable) -> None:
+    """repaly"""
     input_key = "{}:inputs".format(method.__qualname__)
     output_key = "{}:outputs".format(method.__qualname__)
 
     inputs = method.__self__._redis.lrange(input_key, 0, -1)
     outputs = method.__self__._redis.lrange(output_key, 0, -1)
-
-    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
-    for inp, out in zip(inputs, outputs):
+    for input, output in zip(inputs, outputs):
         print(
             "{}(*{}) -> {}".format(
-                method.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
+                method.__qualname__, input.decode("utf-8"),
+                output.decode("utf-8")
+                )
             )
-        )
 
 
 class Cache:
-    """doc doc class"""
-
     def __init__(self):
-        """doc doc method"""
-        self._redis = redis.Redis()
+        self._redis = redis.Redis(host='localhost', port=6379)
         self._redis.flushdb()
 
     @count_calls
     @call_history
-    def store(self, data: Union[str, float, bytes, int]) -> str:
-        """doc doc method"""
-        keyx = str(uuid.uuid4())
-        self._redis.set(keyx, data)
-        return keyx
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis using a random key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
 
-    def get(
-        self, key: str, fn: Optional[Callable] = None
-    ) -> Union[str, bytes, int, float]:
-        value = self._redis.get(key)
+    def get(self, key: str, fn: Optional[Callable] = None
+            ) -> Union[None, str, bytes, int, float]:
+        """convert the data back to the desired format"""
         if fn:
-            value = fn(value)
-        return value
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
 
-    def get_str(self, key: str) -> str:
-        return self.get(key, fn=str)
+    def get_str(self, key: str) -> Union[str, None]:
+        """convert the data back to the desired format"""
+        return self.get(key, str)
 
-    def get_int(self, key: str) -> int:
-        return self.get(key, fn=int)
+    def get_int(self, key: str) -> Union[int, None]:
+        """convert the data back to the desired format"""
+        return self.get(key, int)
