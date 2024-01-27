63e42f4c5b0d98aba5f0a4886b02a798545a1aa3
Laila_Hazmir <125223268+laila22haz@users.noreply.github.com>
2024-01-27 02:32:47 +0100

    ???

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
-
+'''Implementing an expiring web cache and tracker'''
+from functools import wraps
 import redis
 import requests
 from typing import Callable
-from functools import wraps
+import time
 
 _redis = redis.Redis()
 
 
-def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
-
-    @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator guy """
-        _redis.incr(f"count:{url}")
-        cached_response = _redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        _redis.setex(f"cached:{url}", 10, result)
-        return result
-
+def cache_with_expiry(method: Callable) -> Callable:
+    '''Define a decorator that tracks and sets an expired web cache'''
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        _redis.incr("count:{}".format(url))
+        cached = _redis.get("{}".format(url))
+        if cached:
+            return cached.decode("utf-8")
+        response = method(url)
+        _redis.setex("{}".format(url), 10, response)
+        return response
     return wrapper
 
 
-@wrap_requests
+@cache_with_expiry
 def get_page(url: str) -> str:
-    """get page self descriptive
-    """
+    '''Return the content of an HTTP request'''
     response = requests.get(url)
     return response.text
