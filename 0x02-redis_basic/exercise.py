25a0b27780ac040d08e0308c47eb9bb440f54452
Laila_Hazmir <125223268+laila22haz@users.noreply.github.com>
2024-01-27 02:18:25 +0100

    GOOD THINGS TAKE TIME

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -44,6 +44,7 @@ def replay(method: Callable) -> None:
 
     inputs = method.__self__._redis.lrange(input_key, 0, -1)
     outputs = method.__self__._redis.lrange(output_key, 0, -1)
+    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
     for input, output in zip(inputs, outputs):
         print(
             "{}(*{}) -> {}".format(
